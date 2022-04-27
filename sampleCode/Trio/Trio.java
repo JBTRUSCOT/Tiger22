51edf2318cbff346509f9c38636178f03cb4daae
JBTRUSCOT <truscjae000@ephrataschools.org>
2022-04-27 15:31:52 +0000

    tiger

--- sampleCode/Trio/Trio.java
+++ sampleCode/Trio/Trio.java
@@ -1,2 +1,26 @@
 public class Trio implements MenuItem {
+    private Sandwich sandwich;
+    private Salad salad;
+    private Drink drink;
+
+    public trio(Sandwich sandwich, Salad salad, Drink drink) {
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
+    }
+     @Override
+     public String getName() {
+         return this.sandwich.getName()
+         + '/' + this.salad.getName()
+         + '/' + this.drink.getName()
+         + " Trio";
+
+     }
+
+     public double getPrice() {
+         return this.sandwich.getPrice() 
+         + this.salad.getPrice()
+         + this.drink.getPrice();
+     }
 }
+
